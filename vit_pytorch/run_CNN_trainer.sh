2dad6b91b4249eaa02f44ed5ee92d9fec04cdfbd
nkasmanoff <nkasmanoff@gmail.com>
2021-05-09 11:50:56 -0400

    cifar 10 cnn

--- vit_pytorch/run_CNN_trainer.sh
+++ vit_pytorch/run_CNN_trainer.sh
@@ -9,4 +9,4 @@
 #SBATCH --output=slurm-%j.out
 #SBATCH --job-name run_CNN_trainer
 
-python resnet_training_loop.py --gpus 1 --max_epochs 500 --dataset 'cifar100' --num_classes 100
+python resnet_training_loop.py --gpus 1 --max_epochs 500 --dataset 'cifar10' --num_classes 10
