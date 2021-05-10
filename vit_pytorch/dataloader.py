2979099f9164a5189f609e6c451959a27529e05c
nkasmanoff <nkasmanoff@gmail.com>
2021-05-09 22:22:56 -0400

    new norm val

--- vit_pytorch/dataloader.py
+++ vit_pytorch/dataloader.py
@@ -28,30 +28,30 @@ def get_CIFAR_data(dset='cifar10',
                    batch_size = 64,
                    transforms=transforms.Compose([
                     transforms.RandomRotation(degrees=15),
-                  transforms.RandomVerticalFlip(),
+                #  transforms.RandomVerticalFlip(),
                            transforms.ToTensor(),
-                           transforms.Normalize((0.,), (1,))
+                           transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)))
                                    ])):
 
     if dset == 'cifar10':
         dataset = datasets.CIFAR10(root='../data/', download=True, transform=transforms)
         test_dataset = datasets.CIFAR10(root='../data/', train=False, transform=transforms)
-    elif dset == 'cifar100': 
+    elif dset == 'cifar100':
         dataset = datasets.CIFAR100(root='../data/', download=True, transform=transforms)
         test_dataset = datasets.CIFAR100(root='../data/', train=False, transform=transforms)
-        
+
     else:
         print("Must select cifar10 or cifar100")
         sys.exit()
-        
-        
 
-    train_size = len(dataset) - val_size 
+
+
+    train_size = len(dataset) - val_size
 
     train_ds, val_ds = random_split(dataset, [train_size, val_size])
     train_loader = DataLoader(train_ds, batch_size, shuffle=True, num_workers=4, pin_memory=True)
     val_loader = DataLoader(val_ds, batch_size, num_workers=4, pin_memory=True)
     test_loader = DataLoader(test_dataset, batch_size, num_workers=4, pin_memory=True)
-    
-    
+
+
     return train_loader, val_loader, test_loader
