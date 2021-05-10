fa58e334d7b33edcc9777f941a3dd47fd6bff090
Noah Kasmanoff <nsk367@log-1.nyu.cluster>
2021-05-10 12:17:56 -0400

    update jobs

--- vit_pytorch/run_large_trainer.sh
+++ vit_pytorch/run_large_trainer.sh
@@ -9,4 +9,6 @@
 #SBATCH --output=slurm-%j.out
 #SBATCH --job-name run_large_trainer
 
-python training_loop.py --gpus 1 --max_epochs 150 --dataset 'cifar10' --num_classes 10 --architecture 'VitLarge' --depth 24 --heads 16 --dim 1024 --mlp_dim 4096 --dropout .1 --batch_size 128
+python training_loop.py --gpus 1 --max_epochs 150 --dataset 'cifar10' 
+--num_classes 10 --architecture 'VitLarge' --depth 24 --heads 16 --dim 
+1024 --mlp_dim 4096 --dropout 0. --batch_size 128
